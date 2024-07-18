b05e12f9b4c28a1c3120e690603a99f3c67fec05
akash.patel <akash.patel@mindinventory.com>
2024-07-18 17:11:02 +0530

    - open food license and open database licence - micros nutrition information

--- passio-ui-module/src/main/java/ai/passio/nutrition/uimodule/domain/search/SearchUseCase.kt
+++ passio-ui-module/src/main/java/ai/passio/nutrition/uimodule/domain/search/SearchUseCase.kt
@@ -1,6 +1,7 @@
 package ai.passio.nutrition.uimodule.domain.search
 
 import ai.passio.nutrition.uimodule.data.Repository
+import ai.passio.nutrition.uimodule.ui.model.FoodRecord
 import ai.passio.passiosdk.passiofood.PassioFoodDataInfo
 
 object SearchUseCase {
@@ -9,5 +10,12 @@ object SearchUseCase {
 
     suspend fun fetchSearchResults(query: String): Pair<List<PassioFoodDataInfo>, List<String>> {
         return repository.fetchSearchResults(query)
+    }
+
+    suspend fun fetchFoodRecord(passioFoodDataInfo: PassioFoodDataInfo): FoodRecord? {
+        repository.fetchPassioFoodItem(passioFoodDataInfo)?.let {
+            return FoodRecord(it)
+        }
+        return null
     }
 }
