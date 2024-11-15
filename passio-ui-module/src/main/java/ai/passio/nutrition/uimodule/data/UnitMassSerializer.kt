6880eaee3fd9334b28382eb6b267492d4eb86a9a
akash.patel <akash.patel@mindinventory.com>
2024-11-15 18:42:58 +0530

    - Created extension functions to map FoodRecord to iOS data.

--- passio-ui-module/src/main/java/ai/passio/nutrition/uimodule/data/UnitMassSerializer.kt
+++ passio-ui-module/src/main/java/ai/passio/nutrition/uimodule/data/UnitMassSerializer.kt
@@ -10,6 +10,7 @@ import ai.passio.passiosdk.passiofood.data.measurement.Milligrams
 import ai.passio.passiosdk.passiofood.data.measurement.Milliliters
 import ai.passio.passiosdk.passiofood.data.measurement.Ounce
 import ai.passio.passiosdk.passiofood.data.measurement.UnitMass
+import com.google.gson.GsonBuilder
 import com.google.gson.JsonDeserializationContext
 import com.google.gson.JsonDeserializer
 import com.google.gson.JsonElement
@@ -19,19 +20,34 @@ import com.google.gson.JsonSerializer
 import java.lang.reflect.Type
 
 
+private val mGson = GsonBuilder().create()
 class UnitMassSerializer : JsonSerializer<UnitMass>, JsonDeserializer<UnitMass> {
 
-    override fun serialize(src: UnitMass, typeOfSrc: Type, context: JsonSerializationContext): JsonElement {
-        val jsonObject = JsonObject()
+    override fun serialize(
+        src: UnitMass,
+        typeOfSrc: Type,
+        context: JsonSerializationContext
+    ): JsonElement {
+
+
+        /*val jsonObject = JsonObject()
         with(src) {
             jsonObject.addProperty("unit", this.unit.symbol)
             jsonObject.addProperty("value", this.value)
-        }
-        return jsonObject
+        }*/
+        return mGson.fromJson(mGson.toJson(src), JsonObject::class.java) //jsonObject
     }
 
-    override fun deserialize(json: JsonElement, typeOfT: Type, context: JsonDeserializationContext): UnitMass {
+    override fun deserialize(
+        json: JsonElement,
+        typeOfT: Type,
+        context: JsonDeserializationContext
+    ): UnitMass {
         val jsonObject = json as JsonObject
+        if (jsonObject.has("unit") && !jsonObject.get("unit").isJsonPrimitive && jsonObject.get("unit").isJsonObject)
+        {
+            return mGson.fromJson(jsonObject, UnitMass::class.java)
+        }
         val unit = when (val unitString = jsonObject["unit"].asString) {
             "g" -> Grams
             "dag" -> Dekagrams
@@ -45,23 +61,23 @@ class UnitMassSerializer : JsonSerializer<UnitMass>, JsonDeserializer<UnitMass> 
             else -> throw IllegalArgumentException("No known unit of mass: $unitString")
         }
         return UnitMass(unit, jsonObject["value"].asDouble)
-            /*val jsonObject = json.asJsonObject
+        /*val jsonObject = json.asJsonObject
 
-            // Deserialize the 'unit' field manually
-            val unitJson = jsonObject.get("unit")
-            val unit: ai.passio.passiosdk.passiofood.data.measurement.Unit = context.deserialize(unitJson, ai.passio.passiosdk.passiofood.data.measurement.Unit::class.java)
+        // Deserialize the 'unit' field manually
+        val unitJson = jsonObject.get("unit")
+        val unit: ai.passio.passiosdk.passiofood.data.measurement.Unit = context.deserialize(unitJson, ai.passio.passiosdk.passiofood.data.measurement.Unit::class.java)
 
-            // Deserialize the 'value' field
-            val value = jsonObject.get("value").asDouble
+        // Deserialize the 'value' field
+        val value = jsonObject.get("value").asDouble
 
-            return UnitMass(unit, value)*/
-        }
+        return UnitMass(unit, value)*/
+    }
 
 
 }
+/*
 
 
-// Custom deserializer for Unit
 class UnitDeserializer : JsonDeserializer<ai.passio.passiosdk.passiofood.data.measurement.Unit> {
 
     override fun deserialize(
@@ -72,9 +88,18 @@ class UnitDeserializer : JsonDeserializer<ai.passio.passiosdk.passiofood.data.me
         val jsonObject = json.asJsonObject
         val symbol = jsonObject.get("symbol").asString
 
-        // You can handle the Converter part if needed
-        // Example for converter: val converter = context.deserialize(jsonObject.get("converter"), Converter::class.java)
+        var converter = ai.passio.passiosdk.passiofood.data.measurement.Converter()
+        if (jsonObject.has("converter") && jsonObject.getAsJsonObject("converter")
+                .has("coefficient")
+        ) {
+            converter = ai.passio.passiosdk.passiofood.data.measurement.Converter(
+                jsonObject.getAsJsonObject("converter").get("coefficient").asDouble
+            )
+        }
 
-        return ai.passio.passiosdk.passiofood.data.measurement.Unit(symbol = symbol) // Adjust if you need to handle Converter as well
+        return ai.passio.passiosdk.passiofood.data.measurement.Unit(
+            symbol = symbol,
+            converter = converter
+        )
     }
-}
+}*/
