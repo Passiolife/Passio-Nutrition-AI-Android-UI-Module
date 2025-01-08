73db2ae57be64db91110a893b269394e1cf04d20
akash.patel <akash.patel@mindinventory.com>
2025-01-08 17:17:16 +0530

    - Hide brand name when it is same as food name in the add/edit custom food.

--- passio-ui-module/src/main/java/ai/passio/nutrition/uimodule/ui/foodcreator/FoodCreatorViewModel.kt
+++ passio-ui-module/src/main/java/ai/passio/nutrition/uimodule/ui/foodcreator/FoodCreatorViewModel.kt
@@ -362,6 +362,11 @@ class FoodCreatorViewModel : BaseViewModel() {
 
         customFoodRecord = foodRecord.clone()
 
+        if (customFoodRecord?.name == customFoodRecord?.details)
+        {
+            customFoodRecord?.details = ""
+        }
+
         if (!isEditCustomFood) {
             customFoodRecord?.id = ""
             customFoodRecord?.uuid = ""
@@ -369,14 +374,14 @@ class FoodCreatorViewModel : BaseViewModel() {
         }
 
         _isEditCustomFoodEvent.postValue(isEditCustomFood)
-        _prefillFoodData.postValue(foodRecord)
+        _prefillFoodData.postValue(customFoodRecord!!)
     }
 
     fun setDataFromNutritionFacts(nutritionFactsPair: Pair<PassioNutritionFacts, String>) {
         val nutritionFacts = nutritionFactsPair.first
         this.passioIDEntityType = PassioIDEntityType.nutritionFacts
 
-        Log.d("nutritionFacts====", passioGson.toJson(nutritionFacts))
+//        Log.d("nutritionFacts====", passioGson.toJson(nutritionFacts))
 //        productName = nutritionFactsPair.second
         nutritionFacts.servingQuantity?.let {
             setServingSize(it)
